9952982d6896c34cd99e6ca4bb4e9e3931c0a181
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2025-07-09 19:20:16 +0900

    [update] 一部表示順番の変更

--- Assets/Script/EnemyStatusSettings.cs
+++ Assets/Script/EnemyStatusSettings.cs
@@ -1,29 +1,40 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EnemyStatusSettings", menuName = "Scriptable Objects/EnemyStatusSettings")]
 public class EnemyStatusSettings : ScriptableObject
 {
-    public List<EnemyParam> EnemyParamList = new List<EnemyParam>();
+    [Tooltip("ゲームに登場する敵をすべて個別で作成する。武器や特性が違うものは違う個体と判断する")]public List<EnemyParam> EnemyParamList = new List<EnemyParam>();
+
+    public EnemyParam GetParamByID(string id)
+    {
+        return EnemyParamList.FirstOrDefault(p => p.tankID == id);
+    }
 }
 
 [System.Serializable]
 public class EnemyParam
 {
+    [Header("基本情報")]
+
     [Tooltip("その敵の固有IDを設定")] public string tankID;
     [Tooltip("その敵の名前を設定。画面に表示する")] public string tankName;
-    
+    [SerializeField, Tooltip("そのタンクのプレハブを設定する")] GameObject prefab;
     [SerializeField, Tooltip("最大体力を設定")] int maxHP;
     [SerializeField, Tooltip("戦車の移動速度を設定")] float maxMoveSpeed;
     [SerializeField, Tooltip("装備している武器種を設定")] WeaponType weaponType;
     [SerializeField, Tooltip("攻撃頻度を設定")] float weaponFireRate;
     [SerializeField, Tooltip("攻撃範囲を設定")] float attackRange;
     [SerializeField, Tooltip("行動パターンを設定")] BehaviorType behaviorType;
+    [SerializeField, Tooltip("倒したときのスコアを設定")] int rewardScore;
+
+    [Header("特性設定")]
     [SerializeField, Tooltip("プレイヤーに近づいて自爆を行うかどうか")] bool isSelfDestruct;
     [SerializeField, Tooltip("trueの場合移動しなくなる")] bool isStationary;
-    [SerializeField, Tooltip("倒したときのスコアを設定")] int rewardScore;
-    [SerializeField, Tooltip("そのタンクのプレハブを設定する")] GameObject prefab;
     [SerializeField, Tooltip("その他、特殊な設定")] SpecialFlags specialFlags;
+
+    
 
     #region Properties
 
@@ -46,8 +57,7 @@ public enum WeaponType
     None,
     Cannon,
     MachineGun,
-    TwinCannon,
-    SelfDestruct
+    TwinCannon
 }
 
 public enum BehaviorType
--- Assets/Script/HealthManager.cs
+++ Assets/Script/HealthManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
+
+    [SerializeField]private EnemyStatusSettings enemyStatusSettings;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,6 @@ public class HealthManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
