608c3fd1f3b00fa75693541f2a1d88e8cf2af2f7
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2025-10-08 12:56:53 +0900

    [add] スポーン時のデータ処理の追加

--- Assets/Script/SpawnData.cs
+++ Assets/Script/SpawnData.cs
@@ -5,21 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpawnData1", menuName = "Scriptable Objects/SpawnData1")]
 public class SpawnData : ScriptableObject
 {
-    [Tooltip("ゲーム内でウェーブごとに登場する敵を入力。召喚位置と敵IDをリストに登録する。")]public List<Spawnparam> SpawnParamList = new List<Spawnparam>();
-
-    public Spawnparam GetParamByID(string id)
-    {
-        return SpawnParamList.FirstOrDefault(p => p.spawnTankID == id);
-    }
+    [Tooltip("ゲーム内でウェーブごとに登場する敵を入力。召喚位置と敵IDをリストに登録する。")]
+    public List<SpawnParam> SpawnParamList = new List<SpawnParam>();
 }
 
 [System.Serializable]
-public class Spawnparam
+public class SpawnParam
 {
     [Header("基本情報")]
 
     [Tooltip("召喚する敵のIDを設定")] public string spawnTankID;
-    [SerializeField, Tooltip("そのタンクのプレハブを設定する")] Vector3 spawnArea;
+    [SerializeField, Tooltip("そのタンクの召喚場所を座標で設定")] Vector3 spawnArea;
 
     
 
