b5cfa1f46dd939a153ff03c119bf352b66c3d7ae
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2025-12-19 15:10:22 +0900

    [update]倒された時にスコアが追加される処理の追加、死亡時の処理追加と最大HPの設定をパラメーター取得後に設定

--- Assets/Scripts/Health System/EnemyHealthUI.cs
+++ Assets/Scripts/Health System/EnemyHealthUI.cs
@@ -1,20 +1,23 @@
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyHealthUI : MonoBehaviour
 {
+    EnemyParam _param;
     [SerializeField] private HealthManager _healthManager;
     [SerializeField] private GameObject _hpBarRoot; // Canvas付きのオブジェクト
     [SerializeField] private Slider _hpSlider;
+    [SerializeField] private ScoreText _scoreText;
+    int _addScore = 0;
     private Transform _target = default;
 
     private void Start()
     {
         _hpBarRoot.SetActive(false); // 最初は非表示
         _target = GameObject.FindWithTag("Player").transform;
-
+        _scoreText = GameObject.FindFirstObjectByType<ScoreText>().GetComponent<ScoreText>();
         _healthManager.OnHealthChanged.AddListener(UpdateUI);
-        _healthManager.OnDeath.AddListener(HandleDeath);
     }
 
     private void Update()
@@ -22,6 +25,7 @@ public class EnemyHealthUI : MonoBehaviour
         Vector3 p = Camera.main.transform.position;
         p.y = _hpBarRoot.transform.position.y;
         _hpBarRoot.transform.LookAt(p);
+        
     }
 
     private void UpdateUI(int current, int max)
@@ -33,6 +37,15 @@ public class EnemyHealthUI : MonoBehaviour
 
     private void HandleDeath()
     {
+        _scoreText.AddScore = _addScore;
+        Debug.Log($"Score Added: {_addScore}");
         Destroy(gameObject); // 敵を消す
+    }
+    public void Initialize(EnemyParam enemyParam)
+    {
+        _param = enemyParam;
+        _addScore = _param.RewardScore;
+        _hpSlider.maxValue = _param.MaxHP;
+        _healthManager.OnDeath.AddListener(HandleDeath);
     }
 }
